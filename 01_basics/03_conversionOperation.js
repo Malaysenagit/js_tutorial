7b9d10582175ebf3bc4ac384f814e73db2dd28ce
Malay Senapati <109528269+Malaysenagit@users.noreply.github.com>
2024-04-13 05:04:05 +0000

    Conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33abc"  //Enter the below input in score and check( null, undefined, true, "Malay")
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+let isloggedIn = null
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+//"" => false
+// "Malay" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
