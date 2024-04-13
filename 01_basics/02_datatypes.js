e3ff7371459d2212132c1859cca8b464c463602c
Malay Senapati <109528269+Malaysenagit@users.noreply.github.com>
2024-04-13 04:00:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) //we are using node.js not browser
+
+console.log(3 
++
+3) // code readability should be high
+
+console.log("Malay")
+
+// Documentation MDN but, MDN documentation is not a original documentation (MDN documentation mozila organization ke taraf se hai) https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+//original JS documentation tc39.es ECMA script https://tc39.es/ecma262/
+
+let name = "Malay"
+let age = 23
+let isloggedIn = false
+let state;
+
+//number => 2 to power 53
+// bigint 
+// string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof "Malay")
+console.log(typeof age)
+console.log(typeof null)  //object
+console.log(typeof undefined) //undefined
