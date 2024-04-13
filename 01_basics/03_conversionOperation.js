5f7b692e8427d7e4d3eb17e75e2076135a16f9c0
Malay Senapati <109528269+Malaysenagit@users.noreply.github.com>
2024-04-13 07:45:24 +0000

    #

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 let isloggedIn = null
 
 let booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 //"" => false
@@ -22,5 +22,36 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//-----------------------OPERATIONS---------------------
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Hello"
+let str2 = " Malay"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)        //Link to Study https://tc39.es/ecma262/#sec-abstract-operations
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(3 + 2 * 4 -5) // this is not a good way to write a code. Always write a code simple clean and Readable
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;        //Link to study
+++gameCounter;            //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+console.log(gameCounter);
